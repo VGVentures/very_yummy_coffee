2f136c6f7360403f7c2b6eb980a704bf2dd678ba
Thierry Oliveira <thierry.oliveira@verygood.ventures>
2026-01-08 23:39:14 -0300

    test: ensure total coverage for the [main]

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,9 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:very_yummy_coffee/main.dart';
+import 'package:very_yummy_coffee/main.dart' as app;
 
 void main() {
   testWidgets('should MainApp contain a [Hello World!] text', (tester) async {
-    await tester.pumpWidget(const MainApp());
+    await tester.pumpWidget(const app.MainApp());
+
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+
+  testWidgets('startApp runs and mounts MainApp', (tester) async {
+    app.startApp();
+
+    await tester.pump();
+
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+
+  testWidgets('should [startApp] starts the app', (WidgetTester tester) async {
+    app.startApp();
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+
+  testWidgets('library main() calls startApp', (tester) async {
+    app.main();
+
+    await tester.pumpAndSettle();
 
     expect(find.text('Hello World!'), findsOneWidget);
   });
