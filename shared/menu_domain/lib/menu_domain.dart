0821943cab663f2eb88326b4bda2f5a5ab2d7e21
Marcus Twichel <marc.twichy@gmail.com>
2026-02-02 16:42:59 -0700

    feat: add project structure (#6)
    * feat: add project structure
    
    * fix spelling
    
    * fixes
    
    * fix spellign

---
+++ shared/menu_domain/lib/menu_domain.dart
@@ -0,0 +1,4 @@
+/// A repository managing the menu domain.
+library;
+
+export 'src/menu_domain.dart';
---
+++ shared/menu_domain/lib/src/menu_domain.dart
@@ -0,0 +1,7 @@
+/// {@template menu_domain}
+/// A repository managing the menu domain.
+/// {@endtemplate}
+class MenuDomain {
+  /// {@macro menu_domain}
+  const MenuDomain();
+}
