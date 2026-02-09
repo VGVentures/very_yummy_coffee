843c7721d5d92ff00a56011cf3ce3752c3719d22
igorlmgx <40547661+igorlmgx@users.noreply.github.com>
2026-02-09 16:30:18 -0300

    chore: update widgetbook

--- shared/very_yummy_coffee_ui/lib/very_yummy_coffee_ui.dart
+++ shared/very_yummy_coffee_ui/lib/very_yummy_coffee_ui.dart
@@ -3,5 +3,5 @@ library;
 
 export 'src/atoms/atoms.dart';
 export 'src/molecules/molecules.dart';
+export 'src/theme/coffee_theme.dart';
 export 'src/tokens/tokens.dart';
-export 'src/very_yummy_coffee_ui.dart';
--- shared/very_yummy_coffee_ui/test/src/very_yummy_coffee_ui_test.dart
+++ shared/very_yummy_coffee_ui/test/src/very_yummy_coffee_ui_test.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:very_yummy_coffee_ui/very_yummy_coffee_ui.dart';
+import 'package:very_yummy_coffee_ui/src/very_yummy_coffee_ui.dart';
 
 void main() {
   group('VeryYummyCoffeeUi', () {
